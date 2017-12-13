ab4b57cbf156c7a4a0650cb467a1a70d0b62fcd4
Park Jong in <whddls9632@naver.com>
2017-12-14 01:24:44 +0900

    ..

--- run.c
+++ run.c
@@ -18,27 +18,36 @@ p_meta find_meta(p_meta *last, size_t size) {
   p_meta result = base;
   p_meta pbrk = sbrk(0);
   struct rlimit limit;
-  if( getrlimit(RLIMIT_DATA, &limit) != 0 ) exit(0);
+  getrlimit(RLIMIT_DATA, &limit);
+  long hsize = limit.rlim_max/(long)sizeof(void*);
+  void* hend = base + hsize;
   
   switch(fit_flag){
     case FIRST_FIT:
     {
      bool i = true;
      while(i) {
-      previous = index;
       if( index->free == true ) {
        if( index == pbrk ) {
+	last = &previous;
 	result = index;
         i = false;
        }
        else if( size <= index->size ) {
+        last = &previous;
         result = index;
         i = false;
        }
-       else index = index + index->size + sizeof(struct metadata);
+       else { 
+        previous = index;
+        index = index + index->size + sizeof(struct metadata);
        }
-      else if( index >= limit.rlim_max ) return NULL;
-      else index = index + index->size + sizeof(struct metadata);
+      }
+      else if( (void*)index >= hend ) return NULL;
+      else { 
+       previous = index;
+       index = index + index->size + sizeof(struct metadata);
+      }
      }
     }
     break;
@@ -56,33 +65,36 @@ p_meta find_meta(p_meta *last, size_t size) {
     break;
 
   }
-  last = &previous;
   return result;
 }
 
 void *m_malloc(size_t size) {
   p_meta last = base;
-  p_meta previous = base;
+  p_meta now = base;
   p_meta pbrk = sbrk(0);
   struct rlimit limit;
-  p_meta hlimit = ( base + limit.rlim_max );
+  getrlimit(RLIMIT_DATA ,&limit);
+  long hsize = limit.rlim_max/(long)sizeof(void*);
+  void* hend = base + hsize;
   
-  last = find_meta(&previous,size);
-  if( last >= hlimit ) return NULL;
-  if( last == pbrk ) {
-   if( ( hlimit - pbrk ) < size ) return NULL;
+  now = find_meta(&last,size);
+  if( now >= hsize ) return NULL;
+  if( now == pbrk ) {
+   if( ( (p_meta)hend - pbrk ) < size ) return NULL;
    else {
     pbrk = sbrk(size);
-    last->free = false;
-    last->size = size;
-    last->next = pbrk;
-    last->prev = previous;
+    now->free = false;
+    now->size = size;
+    now->next = pbrk;
+    now->prev = last;
    }
   } else {
-   last->free = false;
-   last->size = size;
-   last->next = last + ( size + sizeof(struct metadata));
-   last->prev = previous;
+   now->free = false;
+   now->size = size;
+   now->next = now + ( size + sizeof(struct metadata));
+   now->prev = last;
+   last->next = now;
+   now->next->prev = now;
   }
   return (last + sizeof(struct metadata));
 }
